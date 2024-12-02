10887c27bf769ca8233e97814f4f363e23dbdc7e
Jeric1231 <153045584+Jeric1231@users.noreply.github.com>
2024-12-02 18:18:30 +0800

    updated home

--- home/views.py
+++ home/views.py
@@ -5,7 +5,14 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def home(request):
-    user = request.user
-    projects = Project.objects.filter(owner=user)
-    projects = projects.distinct()  # Ensure no duplicate projects
-    return render(request, 'home.html', {'projects': projects})
+    # Get projects where user is owner or member
+    owned_projects = Project.objects.filter(owner=request.user)
+    member_projects = Project.objects.filter(projectmember__user=request.user)
+    projects = (owned_projects | member_projects).distinct()
+
+    # Group tasks by project
+    tasks_by_project = {}
+    for project in projects:
+        tasks_by_project[project] = Tasks.objects.filter(project=project)
+
+    return render(request, 'home.html', {'projects': projects, 'tasks_by_project': tasks_by_project})
