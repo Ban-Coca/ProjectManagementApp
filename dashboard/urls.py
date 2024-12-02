f3698485bc5a4df57a204e309d9be9735bea206f
Jeric1231 <153045584+Jeric1231@users.noreply.github.com>
2024-12-02 18:17:59 +0800

    updated dashboard

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,10 +1,8 @@
 from django.urls import path
-from django.views.generic import RedirectView
 from . import views
 
 app_name = 'dashboard'
 
 urlpatterns = [
     path('', views.dashboard_view, name='dashboard_view'),
-    path('<int:project_id>/', views.project_dashboard_view, name='project_dashboard_view'),
 ]
